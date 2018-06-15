db3ec4550142fbe14c7f2fe933b02d0bd2280f34
Sergei Sokolov <ser.sv.sokolov@gmail.com>
2018-06-15 11:53:10 +0300

    Test code refactoring

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static java.util.concurrent.TimeUnit.*;
@@ -14,53 +13,70 @@ import static java.util.concurrent.TimeUnit.*;
 public class GroupCreationTest {
     public static FirefoxDriver wd;
 
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
     @BeforeMethod
     public void setUp() {
-       System.setProperty("webdriver.gecko.driver", "E:\\\\Tools\\geckodriver\\geckodriver.exe");
-       wd = new FirefoxDriver();
-       wd.manage().timeouts().implicitlyWait(10, SECONDS);
-       wd.get("http://localhost/addressbook/group.php");
-       wd.findElement(By.name("user")).click();
-       wd.findElement(By.name("user")).clear();
-       wd.findElement(By.name("user")).sendKeys("admin");
-       wd.findElement(By.name("pass")).click();
-       wd.findElement(By.name("pass")).clear();
-       wd.findElement(By.name("pass")).sendKeys("secret");
-       wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+        System.setProperty("webdriver.gecko.driver", "E:\\\\Tools\\geckodriver\\geckodriver.exe");
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(10, SECONDS);
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.get("http://localhost/addressbook/group.php");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
     @Test
-    public void testGroupCreation()
-    {
-        wd.findElement(By.linkText("Групи")).click();
-        wd.findElement(By.name("new")).click();
+    public void testGroupCreation() {
+        goToGroupPage();
+        initNewGroup();
+        fillAllFields(new GroupData("test45", "test45", "test45"));
+        submitGroupCreation();
+        goToGroupPage();
+    }
+
+    private void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillAllFields(GroupData groupData) {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("test44");
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("test44");
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("test44");
-        wd.findElement(By.name("submit")).click();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    private void initNewGroup() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    private void goToGroupPage() {
         wd.findElement(By.linkText("Групи")).click();
     }
 
     @AfterMethod
     public void tearDown() {
         wd.quit();
-    }
-
-    public static boolean isAlertPresent(FirefoxDriver wd)
-    {
-        try{
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e)
-        {
-            return false;
-        }
     }
 
 }

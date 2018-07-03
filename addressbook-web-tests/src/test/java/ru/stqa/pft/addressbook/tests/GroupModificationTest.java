2613d6bcd34d226a851dbf958b95df3f5113092d
Sergei Sokolov <ser.sv.sokolov@gmail.com>
2018-07-03 11:57:51 +0300

    Group modification test added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase
@@ -20,11 +21,16 @@ public class GroupModificationTest extends TestBase
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().modifySelectedGroups();
-        app.getGroupHelper().fillAllFields(new GroupData("test45modified", "test45modified", "test45modified"));
+        GroupData group = new GroupData(before.get(before.size()-1).getId(),"test45modified", "test45modified", "test45modified");
+        app.getGroupHelper().fillAllFields(group);
         app.getGroupHelper().updateGroup();
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size(), after.size());
+
+        before.remove(before.size()-1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object> (before), new HashSet<Object>(after));
     }
 
 }

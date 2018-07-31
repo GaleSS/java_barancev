40e95701e0d31d2cbfc0c6a645c2300cf969d223
Sergei Sokolov <ser.sv.sokolov@gmail.com>
2018-07-31 14:15:31 +0300

    Parsing with jcommander added to GroupDataGenerator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generator/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generator/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generator;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -10,29 +13,42 @@ import java.util.ArrayList;
 
 public class GroupDataGenerator {
 
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
 
-        ArrayList<GroupData> groups = generate(count);
-        saveToFile(groups,file);
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+
+    public static void main(String[] args) throws IOException {
+        GroupDataGenerator generator = new GroupDataGenerator();
+        JCommander.Builder jcommander = JCommander.newBuilder().addObject(generator);
+        try {
+            jcommander.build().parse(args);
+        } catch (ParameterException e) {
+            jcommander.build().usage();
+            return;
+        }
+        generator.run();
     }
 
-    private static ArrayList<GroupData> generate(int count)
-    {
+    private void run() throws IOException {
+        ArrayList<GroupData> groups = generate(count);
+        saveToFile(groups, new File(file));
+    }
+
+    private ArrayList<GroupData> generate(int count) {
         ArrayList<GroupData> groups = new ArrayList<>();
-        for (int i=0;i<count;i++)
-        {
-            groups.add(new GroupData().withName(String.format("testName%s",i)).withFooter(String.format("testFooter%s",i)).withHeader(String.format("testHeader%s",i)));
+        for (int i = 0; i < count; i++) {
+            groups.add(new GroupData().withName(String.format("testName%s", i)).withFooter(String.format("testFooter%s", i)).withHeader(String.format("testHeader%s", i)));
         }
         return groups;
     }
 
-    private static void saveToFile(ArrayList<GroupData> groups, File file) throws IOException {
+    private void saveToFile(ArrayList<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
-        for (GroupData group : groups)
-        {
-            writer.write(String.format("%s;%s;%s\n",group.getName(),group.getHeader(),group.getFooter()));
+        for (GroupData group : groups) {
+            writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
         }
         writer.close();
     }
